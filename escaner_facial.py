b9665cfb9e43d3362408c452435530c868245316
MasterCabro200 <72169427+MasterCabro200@users.noreply.github.com>
2020-09-30 16:45:13 -0500

    Add files via upload

---
+++ escaner_facial.py
@@ -0,0 +1,38 @@
+import time
+import colorama
+import os
+
+from colorama import *
+os.system("cls" if os.name=='nt' else "clear")
+
+print(Fore.LIGHTBLUE_EX+"Hecha por Mastercabro200, Modificado por iLocked")
+print("")
+print(Fore.LIGHTRED_EX+"Iniciando Aplicacion")
+print("\n")
+from tqdm import tqdm
+import time
+
+for i in tqdm([1]):
+    time.sleep(2)
+print("\n")    
+time.sleep(3)
+print(Fore.LIGHTYELLOW_EX+"Configurando herramienta")
+print("\n")
+from tqdm import tqdm
+import time
+
+for i in tqdm([1]):
+    time.sleep(2)
+print("\n") 
+time.sleep(3)
+print("")
+print(Fore.LIGHTCYAN_EX+"Herramienta cargada")
+time.sleep(3)
+print("\n") 
+print (Fore.LIGHTMAGENTA_EX+"Troliado mazter no hay un ezcaner facial jasjasjajsa")
+
+nombre1 = 0
+print("")
+print("Presiona Enter para continuar :v")
+
+nombre1 = input()
